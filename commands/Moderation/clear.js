6794182b5192c0b1013edc4e128a2784f9c4d274
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-01 16:59:24 +0100

    Update clear.js

--- commands/Moderation/clear.js
+++ commands/Moderation/clear.js
@@ -2,10 +2,10 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No.");
-  if(!args[0]) return message.channel.send("no");
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("**You Can't Use This Command Kiddo! Because Your Missing Administrator Permission**");
+  if(!args[0]) return message.channel.send("**Please Specify A Number Of Messages You Want To Delete !**");
   message.channel.bulkDelete(args[0]).then(() => {
-  message.channel.send(`Clear ${args[0]} messages.`).then(msg => msg.delete(2000));
+  message.channel.send(`**Deleting ${args[0]} messages... Finished ! You Just Deleted ${args[0]} Messages.** `).then(msg => msg.delete(2000));
 });
 
 }
