5434117a4cdc2cfd131d3c4093a1d9dec82364b4
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-01 12:18:25 +0100

    Update ban.js

--- commands/Moderation/ban.js
+++ commands/Moderation/ban.js
@@ -2,27 +2,27 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-    if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.channel.send("No can do pal!");
+    if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("You Can't Use This Command Kiddo! Because Your Missing Administrator Permission");
     if(args[0] == "help"){
       message.reply("Usage: !ban <user> <reason>");
       return;
     }
     let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!bUser) return message.channel.send("Can't find user!");
-    let bReason = args.join(" ").slice(22);
-    if(bUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be kicked!");
+    let kReason = args.slice(1).join(" ") || "None";
+    if(bUser.hasPermission("ADMINISTRATOR")) return message.channel.send("**This User Cant Be Kicked Because He Had A Role Highler Than You** ``OR`` **He Is The Owner Of This Server** ``OR`` **He Have Manage ADMINISTRATOR Permission**");
 
     let banEmbed = new Discord.RichEmbed()
     .setDescription("~Ban~")
-    .setColor("#bc0000")
+    .setColor("RANDOM")
     .addField("Banned User", `${bUser} with ID ${bUser.id}`)
     .addField("Banned By", `<@${message.author.id}> with ID ${message.author.id}`)
     .addField("Banned In", message.channel)
     .addField("Time", message.createdAt)
     .addField("Reason", bReason);
 
-    let incidentchannel = message.guild.channels.find(`name`, "incidents");
-    if(!incidentchannel) return message.channel.send("Can't find incidents channel.");
+    let logchannel = message.guild.channels.find(`name`, "log");
+    if(!logchannel) return message.channel.send("**Can't find log channel. Please Create A New Channel With log Name To Be Able To Use This Command**");
 
     message.guild.member(bUser).ban(bReason);
     incidentchannel.send(Embed);
