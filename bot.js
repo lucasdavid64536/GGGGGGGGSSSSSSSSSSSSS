99108f80d7a20c9b37c4abd632896c0efdd6f191
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-01 10:30:52 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -36,11 +36,7 @@ client.on('message', msg => {
       msg.delete();
       msg.author.send('``Anti Bad Wors Blocked`` 🍂 **Stop what you are Posting this Action might have been Logged. Stop Saying Bad Words !**');
     }
-client.on('message', msg => {
-  const swearWords = ["yt", "youtube",".help",".h",".avatar",".ban",".kick",".serverinfo",".info",".8ball",".game",".invite",".count",".ping",".p",".prefix",".support"];
-      msg.author.send('``Do You Like My Commands ? Do You Like Me ??`` 🍂 **Follow My Owner In Hes Youtube Channel & Learn How To Make An Awesome Discord Bot Like Me Dont Forget To Subscribe To Him And Watch All Hes Videos**');
-    }	
-	
+
 });
 client.login(process.env.token);
 
