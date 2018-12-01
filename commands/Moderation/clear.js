8dede136e2b94eebc90a8f65b47b16961db59492
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-01 11:50:22 +0100

    Update clear.js

--- commands/Moderation/clear.js
+++ commands/Moderation/clear.js
@@ -7,7 +7,7 @@ module.exports.run = async (bot, message, args) => {
   if(!args[0]) return message.channel.send("oof");
   message.channel.bulkDelete(args[0]).then(() => {
     message.channel.send(`Cleared ${args[0]} messages.`).then(msg => msg.delete(5000));
-  };
+  }
 }
 
 module.exports.help = {
