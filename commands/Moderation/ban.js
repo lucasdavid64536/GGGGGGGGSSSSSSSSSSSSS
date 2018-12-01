1d22776027e27e8bbd921a1c304e30e78510e207
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-01 11:58:18 +0100

    Update ban.js

--- commands/Moderation/ban.js
+++ commands/Moderation/ban.js
@@ -1,19 +1,33 @@
-let Discord = require('discord.js');
+const Discord = require("discord.js");
 
-exports.run = (client, message) => {
-	
-  var member = message.mentions.members.first();
-  if(!message.member.permissions.has('ADMINISTRATOR')) return message.reply('**You Cant Use This Command Because You Dont Have Permissions To Ban Members !**')
-  member.ban();
-  message.reply('**This Member Got Banned By You For Breaking Rules... He Is gone ....** ``RIP``')
-  message.react('ðŸ‘Œ');
-	
+module.exports.run = async (bot, message, args) => {
+
+    if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.channel.send("No can do pal!");
+    if(args[0] == "help"){
+      message.reply("Usage: !ban <user> <reason>");
+      return;
+    }
+    let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!bUser) return message.channel.send("Can't find user!");
+    let bReason = args.join(" ").slice(22);
+    if(bUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be kicked!");
+
+    let banEmbed = new Discord.RichEmbed()
+    .setDescription("~Ban~")
+    .setColor("#bc0000")
+    .addField("Banned User", `${bUser} with ID ${bUser.id}`)
+    .addField("Banned By", `<@${message.author.id}> with ID ${message.author.id}`)
+    .addField("Banned In", message.channel)
+    .addField("Time", message.createdAt)
+    .addField("Reason", bReason);
+
+    let incidentchannel = message.guild.channels.find(`name`, "incidents");
+    if(!incidentchannel) return message.channel.send("Can't find incidents channel.");
+
+    message.guild.member(bUser).ban(bReason);
+    incidentchannel.send(banEmbed);
 }
+
 module.exports.help = {
-	name: "ban",
-	alias: [
-	"BAN",
-	"banhammer",
-	"getGhost"
-	]
+  name:"ban"
 }
