7b9ffd8362357361c2644a8ddd0d4a76b45f8600
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-01 11:57:41 +0100

    Update kick.js

--- commands/Moderation/kick.js
+++ commands/Moderation/kick.js
@@ -1,19 +1,33 @@
-let Discord = require('discord.js');
+const Discord = require("discord.js");
 
-exports.run = (client, message) => {
-	
-  var member = message.mentions.members.first();
-  if(!message.member.permissions.has('ADMINISTRATOR'))return message.reply('**You Cant Use This Command Because You Dont Have Permissions To Kick Members !**')
-  member.kick();
-  message.reply('**This Member Got Kicked By You For Breaking Rules... He Is gone ....** ``RIP``')
-  message.react('ðŸ‘Œ');
-	
+module.exports.run = async (bot, message, args) => {
+
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("No can do pal!");
+    if(args[0] == "help"){
+      message.reply("Usage: !kick <user> <reason>");
+      return;
+    }
+    let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!kUser) return message.channel.send("Can't find user!");
+    let kReason = args.join(" ").slice(22);
+    if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be kicked!");
+
+    let kickEmbed = new Discord.RichEmbed()
+    .setDescription("~Kick~")
+    .setColor("#e56b00")
+    .addField("Kicked User", `${kUser} with ID ${kUser.id}`)
+    .addField("Kicked By", `<@${message.author.id}> with ID ${message.author.id}`)
+    .addField("Kicked In", message.channel)
+    .addField("Tiime", message.createdAt)
+    .addField("Reason", kReason);
+
+    let kickChannel = message.guild.channels.find(`name`, "incidents");
+    if(!kickChannel) return message.channel.send("Can't find incidents channel.");
+
+    message.guild.member(kUser).kick(kReason);
+    kickChannel.send(kickEmbed);
 }
+
 module.exports.help = {
-	name: "kick",
-	alias: [
-	"KICK",
-	"kik",
-	"KIK"
-	]
+  name:"kick"
 }
