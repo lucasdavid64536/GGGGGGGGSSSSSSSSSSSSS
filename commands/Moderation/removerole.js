68b4375069ef50b535bdf0f54b740822ed5406ea
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-01 12:38:47 +0100

    Create removerole.js

---
+++ commands/Moderation/removerole.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+  if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("**You Can't Use This Command Kiddo! Because Your Missing Administrator Permission**");
+  let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+  if(!rMember) return message.reply("**I Can't Find This User ! Mention The User To Be Able To Use This Command**");
+  let role = args.join(" ").slice(22);
+  if(!role) return message.reply("**Please Specify a role! === Put The Role Name But Don't Mention It Just The Name !**");
+  let gRole = message.guild.roles.find(`name`, role);
+  if(!gRole) return message.reply("**i Cant Find This Role.. Try An Other Name Or Check If You Wrote The Correct Name Of The Role**");
+
+  if(!rMember.roles.has(gRole.id)) return message.reply("**This Member Doest Have This Role **");
+  await(rMember.removeRole(gRole.id));
+
+  try{
+    await rMember.send(`RIP, you lost the ${gRole.name} role.`)
+  }catch(e){
+    message.channel.send(`RIP to <@${rMember.id}>, We removed ${gRole.name} from them. We tried to DM them, but their DMs are locked.`)
+  }
+}
+
+module.exports.help = {
+  name: "removerole"
+}
