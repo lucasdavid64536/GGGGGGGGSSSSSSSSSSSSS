de6a88b17e22705d42c95decfc0c5638fd2ebf35
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-01 12:34:52 +0100

    Update addrole.js

--- commands/Moderation/addrole.js
+++ commands/Moderation/addrole.js
@@ -3,26 +3,26 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
   //!addrole @andrew Dog Person
-  if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("Sorry pal, you can't do that.");
+  if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("**You Can't Use This Command Kiddo! Because Your Missing Administrator Permission**");
   if(args[0] == "help"){
     message.reply("Usage: !addrole <user> <role>");
     return;
   }
   let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-  if(!rMember) return message.reply("Couldn't find that user, yo.");
+  if(!rMember) return message.reply("**I Can't Find This User ! Mention The User To Be Able To Use This Command**");
   let role = args.join(" ").slice(22);
   if(!role) return message.reply("Specify a role!");
   let gRole = message.guild.roles.find(`name`, role);
   if(!gRole) return message.reply("Couldn't find that role.");
 
-  if(rMember.roles.has(gRole.id)) return message.reply("They already have that role.");
+  if(rMember.roles.has(gRole.id)) return message.reply("**This Member Had Already This Role So He Don't Need It From You** ");
   await(rMember.addRole(gRole.id));
 
   try{
-    await rMember.send(`Congrats, you have been given the role ${gRole.name}`)
+    await rMember.send(`**Congratulation You Got A New Special Role Enjoy It ! This Is Your Role** ${gRole.name}`)
   }catch(e){
     console.log(e.stack);
-    message.channel.send(`Congrats to <@${rMember.id}>, they have been given the role ${gRole.name}. We tried to DM them, but their DMs are locked.`)
+    message.channel.send(`Congratulation to <@${rMember.id}>, He Got A New Special Role  ${gRole.name}. I Sent Him A Full Congrat Letter`)
   }
 }
 
