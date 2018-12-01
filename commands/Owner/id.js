aeef22de2506894e7db81791b0f10ef59987b1dd
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-01 11:03:49 +0100

    Create id.js

---
+++ commands/Owner/id.js
@@ -0,0 +1,12 @@
+let Discord = require('discord.js');
+
+exports.run = (client, message) => {
+	
+  msg.channel.send(`${msg.guild.id}`)
+  
+}
+
+module.exports.help = {
+	name: "id",
+	alias: "ID"
+}
