a7713d913ec0a3b3bd92445ee43260f601c96238
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-01 17:20:08 +0100

    Update ban.js

--- commands/Moderation/ban.js
+++ commands/Moderation/ban.js
@@ -21,11 +21,11 @@ module.exports.run = async (bot, message, args) => {
     .addField("Time", message.createdAt)
     .addField("Reason", bReason);
 
-    let incidentchannel = message.guild.channels.find(`name`, "log");
+let incidentchannel = message.guild.channels.find(`name`, "log");
     if(!incidentchannel) return message.channel.send("**Can't find log channel. Please Create A New Channel With log Name To Be Able To Use This Command**");
 
     message.guild.member(bUser).ban(bReason);
-    logchannel.send(Embed);
+    incidentchannel.send(banEmbed);
 }
 
 module.exports.help = {
