f73107a89d590f26d6e781e5465794b0cf887fca
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-01 10:40:28 +0100

    Update dm.js

--- commands/Moderation/dm.js
+++ commands/Moderation/dm.js
@@ -1,17 +1,17 @@
 let Discord = require('discord.js');
 
-exports.run = (client, message) => {
-let id = messge.content.split(' ').slice(1,2).join(' ');
-  let dUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(id);
-  if (!dUser) return message.channel.send("**I Can't Find This User ! You Must Mention Him OR This User Isn't In This Server**")
-  if(!message.member.permissions.has('ADMINISTRATOR')) return message.reply("**you cant Use This Command Because You Dont Have One From Those Roles Administrator``\``Owner``\``Moderator OR You Are Missing Administrator Permission**");
-  let dMsg = messge.content.split(' ').slice(2).join(' ');
-  if(dMsg.length < 1) return message.reply('You must supply a message!')
+exports.run = (client, msg) => {
+let id = msg.content.split(' ').slice(1,2).join(' ');
+  let dUser = msg.guild.member(msg.mentions.users.first()) || msg.guild.members.get(id);
+  if (!dUser) return msg.channel.send("**I Can't Find This User ! You Must Mention Him OR This User Isn't In This Server**")
+  if(!msg.member.permissions.has('ADMINISTRATOR')) return message.reply("**you cant Use This Command Because You Dont Have One From Those Roles Administrator``\``Owner``\``Moderator OR You Are Missing Administrator Permission**");
+  let dMsg = msg.content.split(' ').slice(2).join(' ');
+  if(dMsg.length < 1) return msg.reply('You must supply a message!')
   
-  dUser.send(`${dUser} **This Is Moderator\Administrator\Owner**_${message.author}_**Sent You A Dm From Thier Server  Read It Carefully : ** >> ${dMsg}`)
+  dUser.send(`${dUser} **This Is Moderator\Administrator\Owner**_${msg.author}_**Sent You A Dm From Thier Server  Read It Carefully : ** >> ${dMsg}`)
   
-  message.author.send(`${message.author} You Have Sent This Message <<${dMsg}>> To ${dUser}`)
-  message.react('ðŸ‘Œ');
+  msg.author.send(`${msg.author} You Have Sent This Message <<${dMsg}>> To ${dUser}`)
+  msg.react('ðŸ‘Œ');
 
 }
 module.exports.help = {
