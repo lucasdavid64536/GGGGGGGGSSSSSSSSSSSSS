20cfaefa2a66651b9251e603c932e54c6e0ed6b1
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-01 16:45:08 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,8 +8,6 @@ client.on('ready', () => {
 client.user.setActivity(`${client.guilds.size} SERVERS TYPE .HELP`, {type: 'WATCHING'});
 	setInterval(function() {
 
-  var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
-
   client.user.setGame(oyun[random], "https://www.twitch.tv/");
   }, 10000);
 console.log('IM READY !')
