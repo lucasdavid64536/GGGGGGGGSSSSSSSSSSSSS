53df5bcd132245610d07a0038fb38c4dee9c78f7
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-01 12:42:34 +0100

    Update addrole.js

--- commands/Moderation/addrole.js
+++ commands/Moderation/addrole.js
@@ -11,9 +11,9 @@ module.exports.run = async (bot, message, args) => {
   let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
   if(!rMember) return message.reply("**I Can't Find This User ! Mention The User To Be Able To Use This Command**");
   let role = args.join(" ").slice(22);
-  if(!role) return message.reply("Specify a role!");
+  if(!role) return message.reply("**Please Specify a role! === Put The Role Name But Don't Mention It Just The Name !**");
   let gRole = message.guild.roles.find(`name`, role);
-  if(!gRole) return message.reply("Couldn't find that role.");
+  if(!gRole) return message.reply("**i Cant Find This Role.. Try An Other Name Or Check If You Wrote The Correct Name Of The Role**");
 
   if(rMember.roles.has(gRole.id)) return message.reply("**This Member Had Already This Role So He Don't Need It From You** ");
   await(rMember.addRole(gRole.id));
