9e07f3356ce32bd46e45558f407b5e331687ec21
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-02 13:03:37 +0100

    Update bans.js

--- commands/Information/bans.js
+++ commands/Information/bans.js
@@ -2,11 +2,8 @@ let Discord = require('discord.js');
 
 exports.run = (client, message) => {
 	
-  let embed = new Discord.RichEmbed()
-  .setColor("RANDOM")
-  .setTitle('**Total Bans**')
-  .addField("**Total Members Who They Got Bans In This Server Are**", fetchBans())
-message.channel.send(embed)
+  message.reply(`Total Bans** ${fetchBans}`)
+	
 message.react('😂');
 	
 }
