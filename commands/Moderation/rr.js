b7a8eb9d379529724950dd6326f106ae733ac2a7
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-02 10:23:07 +0100

    Update rr.js

--- commands/Moderation/rr.js
+++ commands/Moderation/rr.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
   if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("**You Can't Use This Command Kiddo! Because Your Missing Administrator Permission**");
   let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
   if(!rMember) return message.reply("**I Can't Find This User ! Mention The User To Be Able To Use This Command**");
-  let role = message.guild.role(message.mentions.roles.first()) || message.guild.roles.get(args[0]);
+  let role = message.guild.roles(message.mentions.roles.first()) || message.guild.roles.get(args[0]);
   if(!role) return message.reply("**Please Specify a role! === Put The Role Name But Don't Mention It Just The Name !**");
   let gRole = message.guild.roles.find(`name`, role);
   if(!gRole) return message.reply("**i Cant Find This Role.. Try An Other Name Or Check If You Wrote The Correct Name Of The Role**");
