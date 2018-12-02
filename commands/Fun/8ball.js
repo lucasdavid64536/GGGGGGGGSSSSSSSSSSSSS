97ff22a7987abc00d1c51e449753ac9cfe53d2f3
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-02 10:30:22 +0100

    Create 8ball.js

---
+++ commands/Fun/8ball.js
@@ -0,0 +1,35 @@
+//Just The arrays/list of available answers
+var list = [
+    'It is certain',
+    'It is decidedly so',
+    'Without a doubt',
+    'Yes definitely',
+    'You may rely on it',
+    'As I see it, yes',
+    'Most likely',
+    'Outlook good',
+    'Yes',
+    'Signs point to yes',
+    'Reply hazy try again',
+    'Ask again later',
+    'Better not tell you now',
+    'Cannot predict now',
+    'Concentrate and ask again',
+    'Don't count on it',
+    'My reply is no',
+    'My sources say no',
+    'Outlook not so good',
+    'Very doubtful'
+];
+//The randomizer    Math.floor = rounds to the nearest integers     Math.random = Generates random number between a number between 0 and 1      list.length = Grabs the length of the list 
+var rand = Math.floor(Math.random() * list.length);
+
+//Send's a reply to the user who wrote the message
+message.reply(list[rand])
+
+
+}
+module.exports.help = {
+	name: "8ball",
+	alias: "8b"
+}
