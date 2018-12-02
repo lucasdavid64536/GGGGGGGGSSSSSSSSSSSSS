3c04fc096b77ace99cee9c8add653fa85a540176
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-02 12:30:56 +0100

    Update counts.js

--- commands/Information/counts.js
+++ commands/Information/counts.js
@@ -2,11 +2,9 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-let serverembed = new Discord.RichEmbed()
-.setTitle('Total Members')
-addField("**Total Members In This Server Are : **", message.guild.memberCount);
+message.reply('Total Members')
+message.channel.send("**Total Members In This Server Are : **", message.guild.memberCount);
 
-message.channel.send(embed);
 }
 
 module.exports.help = {
