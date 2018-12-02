c4598097bc6082690b30c8073fde1aa85fa17e8c
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-02 15:54:19 +0100

    Update meme.js

--- commands/Fun/meme.js
+++ commands/Fun/meme.js
@@ -1,16 +1,16 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
-  meme(function(data) {
-  const embed = new Discord.RichEmbed()
-  .setTitle(data.title[0])
-  .setColor("RANDOM")
-  .setImage(data.url[0])
-  message.channel.send({embed});
-  })};
+  exports.run = (client, message) => {
+let nickname = args.join(' ')
+  message.guild.members.get('YOUR BOT ID')
+  	.setNickname(nickname);
+  await message.channel.send({
+  	embed: new Discord.RichEmbed()
 
-module.exports.help = {
-	name: "meme",
-	alias: "MEME"
+  		.setTitle(`Changed Server Nickname to ${nickname}`)
+  })
+  }
+  module.exports.help = {
+	name: "set server",
 }
