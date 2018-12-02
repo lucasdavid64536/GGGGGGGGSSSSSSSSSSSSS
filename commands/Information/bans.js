69cdd199cb8e84f9c20341474119b92c885e5ecd
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-02 12:54:06 +0100

    Create bans.js

---
+++ commands/Information/bans.js
@@ -0,0 +1,17 @@
+let Discord = require('discord.js');
+
+exports.run = (client, message) => {
+	
+  let embed = new Discord.RichEmbed()
+  .setColor("RANDOM")
+  .setTitle('**Total Bans**')
+  .addField("**Total Members Who They Got Bans In This Server Are**", message.guild.bansCount)
+message.channel.send(embed)
+message.react('ðŸ‘');
+	
+}
+module.exports.help = {
+	name: "bans",
+  alias: "i bans"
+}
+	
