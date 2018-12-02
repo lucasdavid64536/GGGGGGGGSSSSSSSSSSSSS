0d7992454c673796cb616c65e2250c8470363a2b
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-02 12:11:42 +0100

    Create uptime.js

---
+++ commands/Moderation/uptime.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+let totalSeconds = (client.uptime / 1000);
+let hours = Math.floor(totalSeconds / 3600);
+totalSeconds %= 3600;
+let minutes = Math.floor(totalSeconds / 60);
+let seconds = totalSeconds % 60;
+
+let uptime = `${hours} hours, ${minutes} minutes and ${seconds} seconds`;
+
+}
+module.exports.help = {
+  name: "up"
+}
