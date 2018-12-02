5be7ae271981508ae281ebe75d0df074c47c2359
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-02 12:36:09 +0100

    Update counts.js

--- commands/Information/counts.js
+++ commands/Information/counts.js
@@ -3,7 +3,6 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
 message.reply('Total Members' + "**Total Members In This Server Are : **", message.guild.memberCount);
-"**Total Members In This Server Are : **", message.guild.memberCount)
 
 }
 
