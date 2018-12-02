c2539f08b9571ccb7a03dc2a1437ee4d856e4f4e
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-02 12:17:19 +0100

    Update uptime.js

--- commands/Moderation/uptime.js
+++ commands/Moderation/uptime.js
@@ -9,7 +9,7 @@ let minutes = Math.floor(totalSeconds / 60);
 let seconds = totalSeconds % 60;
 
 let uptime = `${hours} hours, ${minutes} minutes and ${seconds} seconds`;
-message.channel.send(`**uptime**`)
+message.channel.send(uptime)
 }
 module.exports.help = {
   name: "up"
