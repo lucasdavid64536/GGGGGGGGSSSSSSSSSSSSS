bdf4fd3161599cd0b8b1daa86cd85dae0f4e2e5e
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-03 16:41:35 +0100

    Update heat.js

--- commands/Fun/heat.js
+++ commands/Fun/heat.js
@@ -3,8 +3,7 @@ let Discord = require('discord.js');
 exports.run = (client, message) => {
 	
   message.delete();
-  message.reply(`*** Heating ${message.author}...*** :hotsprings: :hotsprings:`)
-	setTimeout(10000)(m => m.edit(`Now ${message.author} Is Hot :hotsprings: `) );
+  message.reply(`*** Heating ${message.author}...*** :hotsprings: :hotsprings:`).then(m => m.edit(`Now ${message.author} Is Hot :hotsprings: `) );
 }
 
 module.exports.help = {
