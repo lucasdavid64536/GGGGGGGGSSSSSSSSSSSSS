cfd5e7c2849bab37a67816fd6a99d842dac80330
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-03 16:44:42 +0100

    Update heat.js

--- commands/Fun/heat.js
+++ commands/Fun/heat.js
@@ -5,6 +5,11 @@ exports.run = (client, message) => {
   message.delete();
   message.reply(`*** Heating ${message.author}...*** :hotsprings: :hotsprings:`).then(m => m.edit(`Now ${message.author} Is Hot :hotsprings: `) );
 }
+message.channel.send(`*** Heating ${message.author}...*** :hotsprings: :hotsprings:`).then(m => m.edit(`Now ${message.author} Is Hot :hotsprings: `) );
+            setTimeout(function() {
+                //code
+            }, 10000);
+        });
 
 module.exports.help = {
 	name: "heat",
