79ab942936a64ef16e0b16c4571ae6aee777d03f
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-03 16:18:12 +0100

    Create heat.js

---
+++ commands/Fun/heat.js
@@ -0,0 +1,12 @@
+let Discord = require('discord.js');
+
+exports.run = (client, message) => {
+
+message.reply(`*** Heating ${message.author}***:hotsprings: :hotsprings: `).then(m => m.edit(`Now ${message.author} Is Hot :hotsprings: `);
+
+}
+
+module.exports.help = {
+	name: "heat",
+	alias: "HEAT"
+}
