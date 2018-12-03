543cc07e7d5fa44b0d39a54bf1b7a9964cece01f
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-03 17:38:20 +0100

    Update botinfo.js

--- commands/Information/botinfo.js
+++ commands/Information/botinfo.js
@@ -6,14 +6,14 @@ module.exports.run = async (bot, message, args) => {
     .setDescription("Bot Information")
     .setColor("#15f153")
     .setThumbnail(bicon)
-    .addField("**Bot Name**", ```bot.user.username```)
-    .addField("**My Owner**", ```Ban#9153```)
-    .addField("**Discord.js**", ```v11.4.2```)
-    .addField("**Created On**", ```bot.user.createdAt```)
-    .addField("**Servers Bot Is In Them**", ```${client.guilds.size}```)
-    .addField("**Watching Member**", ```${client.users.size}```)
-    .addField("**Playing With Bots**", ```${client.bots.size}```)
-    .addField("**Up Time**", ```${hours} hours, ${minutes} minutes and ${seconds} seconds```)
+    .addField('**Bot Name**', ```bot.user.username```)
+    .addField('**My Owner**', ```Ban#9153```)
+    .addField('**Discord.js**', ```v11.4.2```)
+    .addField('**Created On**', ```bot.user.createdAt```)
+    .addField('**Servers Bot Is In Them**', ```${client.guilds.size}```)
+    .addField('**Watching Member**', ```${client.users.size}```)
+    .addField('**Playing With Bots**', ```${client.bots.size}```)
+    .addField('**Up Time**', ```${hours} hours, ${minutes} minutes and ${seconds} seconds```)
     
 
     message.channel.send(botembed);
