ed25ac9b77efb9ac3997936347f73fcfc9fc9f5a
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-03 15:51:24 +0100

    Create slap.js

---
+++ commands/Fun/slap.js
@@ -0,0 +1,12 @@
+let Discord = require('discord.js');
+
+exports.run = (client, message) => {
+
+message.reply(`***slaps ${message.mentions.members.first()}***`)
+
+}
+
+module.exports.help = {
+	name: "slap",
+	alias: "SLAP"
+}
