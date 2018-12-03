437807b15d4a8d7a2212cec001d3b6803767fc63
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-03 17:17:30 +0100

    Update info.js

--- commands/Information/info.js
+++ commands/Information/info.js
@@ -20,19 +20,7 @@ if(!cmd){
   message.channel.send(embed);
   message.react("✅");
 }	
-if(cmd === ('server'||'sv')){
-  let embed = new Discord.RichEmbed()
-  .setColor('RANDOM')
-  .setTitle('**Server Info**')
-  .addField("Id", message.guild.id)
-  .addField("Owner", message.guild.owner)
-  .addField("Owner Id", message.guild.ownerID)
-  .addField("Roles", message.guild.roles.size)
-  .addField("Region", message.guild.region)
-.setThumbnail(message.guild.iconURL)
-message.channel.send(embed)
-message.react('✅');
-}
+
 	
 }
 module.exports.help = {
