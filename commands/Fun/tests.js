2e1f490eb07dc20556afc68e7b018642b5b4271a
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-03 16:54:12 +0100

    Create tests.js

---
+++ commands/Fun/tests.js
@@ -0,0 +1,30 @@
+let Discord = require('discord.js');
+
+exports.run = (client, message) => {
+
+
+    const embed = new RichEmbed()
+      // Set the title of the field
+      .setTitle('Zapper')
+      .setDescription('Chaque jour 10 nouvelles anecdotes à découvrir')
+      // Set the color of the embed
+      .setColor(0xFF0000)
+      // Set the main content of the embed
+      .addField('Vocabulaire', "Nycthémère signifie l'espace de temps d'une durée de 24 heures, alternant nuit jour.", true)
+      
+    message.channel.send(embed)
+    message.react(':yum:')
+  
+      
+      
+      
+}
+
+
+})
+
+module.exports.help = {
+	name: "test",
+	alias: "TEST"
+
+}
