47f6a187fafc6c856c789089c823ea719fb67966
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-03 17:05:49 +0100

    Update dm.js

--- commands/Moderation/dm.js
+++ commands/Moderation/dm.js
@@ -4,7 +4,7 @@ exports.run = (client, msg) => {
 let id = msg.content.split(' ').slice(1,2).join(' ');
   let dUser = msg.guild.member(msg.mentions.users.first()) || msg.guild.members.get(id);
   if (!dUser) return msg.channel.send("**I Can't Find This User ! You Must Mention Him OR This User Isn't In This Server**")
-  if(!msg.member.permissions.has('ADMINISTRATOR')) return message.reply("**you cant Use This Command Because You Dont Have One From Those Roles Administrator``\``Owner``\``Moderator OR You Are Missing Administrator Permission**");
+  if(!msg.member.permissions.has('ADMINISTRATOR')) return message.reply("**you cant Use This Command Because You Dont Have One From Those Roles Administrator**``/``**Owner**``/``**Moderator OR You Are Missing Administrator Permission**");
   let dMsg = msg.content.split(' ').slice(2).join(' ');
   if(dMsg.length < 1) return msg.reply('You must supply a message!')
   
