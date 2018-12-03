088f0af4c97bf5bff8b52909a2e40a226c3dcc54
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-03 16:03:23 +0100

    Update hug.js

--- commands/Fun/hug.js
+++ commands/Fun/hug.js
@@ -3,11 +3,8 @@ let Discord = require('discord.js');
 exports.run = (client, message) => {
 	
 let user = message.mentions.users.first() || message.author;
-var embed = new Discord.RichEmbed()
-.setTitle("**Cute**")
-.addField(`***${message.member.displayName} hugs ${message.mentions.members.first()}***`)
-.setColor("RANDOM")
- message.channel.send(embed)
+	
+message.reply(`***${message.member.displayName} hugs ${message.mentions.members.first()}***`)
 }
 
 module.exports.help = {
