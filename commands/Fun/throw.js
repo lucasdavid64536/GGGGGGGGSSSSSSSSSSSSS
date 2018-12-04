a2c5eed1954dedfbd98a6541865534238391d28e
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-04 12:43:56 +0100

    Update throw.js

--- commands/Fun/throw.js
+++ commands/Fun/throw.js
@@ -2,6 +2,12 @@ let Discord = require('discord.js');
 
 exports.run = (client, message) => {
 
+let memberToFind = message.mentions.members.first();
+
+  if (!memberToFind) {
+    return message.channel.send('**You Must Mention A User First To Use This Command** ***Expample :*** ``.throw @User``');
+  }
+	
 message.reply(`***throws ${message.mentions.members.first()}***`)
 
 }
