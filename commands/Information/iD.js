dd6b1c08940b2f9c8976cdcb6ccf33114eaa1056
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-04 13:36:10 +0100

    Update iD.js

--- commands/Information/iD.js
+++ commands/Information/iD.js
@@ -13,4 +13,9 @@ let cmd = message.content.split(' ').slice(1, 2).join(' ');
    var embed = new Discord.RichEmbed()
    .setTitle("**Member ID:**")
    .setDescription("**This Member ID Is:**", memberToFind.id, true)
+   }
+module.exports.help = {
+  name:"id"
+}
+
   
