016bef7918ad1dd8f4be73a798764aa1306606ca
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-04 14:57:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -65,7 +65,19 @@ module.exports = (client, guild) => {
     .setTimestamp();
     guildCreateJoin.send(joinEmbed);
 }
-
+module.exports = (client, guild) => {
+    let guildCreateDelete = client.channels.get('517388949048655892');
+    let leaveEmbed = new Discord.RichEmbed()
+    .setTitle("Bot left server!")
+    .setThumbnail(guild.iconURL)
+    .addField(`Server Name:`, `${guild.name}`)
+    .addField(`Server ID:`, `${guild.id}`)
+    .addField(`Server Owner:`, `${guild.owner}`)
+    .setColor("#4286f4")
+    .setFooter(`${client.config.botname}`)
+    .setTimestamp();
+    guildCreateDelete.send(leaveEmbed);
+}
         
 });
   client.login(process.env.token);
