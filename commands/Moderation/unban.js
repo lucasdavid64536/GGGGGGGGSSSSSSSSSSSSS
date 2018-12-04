1584176776b6fd4a3a262d64a4a0ef12735bbd10
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-04 14:23:01 +0100

    Update unban.js

---
+++ commands/Moderation/unban.js
@@ -0,0 +1,22 @@
+exports.run = (bot, message, args) => {
+    const reason = args.slice(1).join(' ');
+    bot.unbanReason = reason;
+    bot.unbanAuth = message.author;
+    const user = args[0];
+    const modlog = bot.channels.find('name', 'log');
+    if (!modlog) return message.reply('**I Cant Find Log Channel Please Create A Channel With** ``log`` **Name To Be Able To Use This Command**');
+    if (reason.length < 1) return message.reply('*You Did Not Specify The Reason For The Unban **');
+    if (!user) return message.reply('**I Cant Find User ! Please Type Hes** ``__ID__`` **And This Command Will Work**').catch(console.error);
+    message.guild.unban(user);
+    message.reply(`Successfuly unbanned <@${user}>`)
+    var embed = new Discord.RichEmbed()
+    .setColor("RANDOM")
+    .setTimestamp()
+    .addField('Action:', 'Unban')
+    .addField('User:', `${user.username}#${user.discriminator} (${user.id})`)
+    .addField('Authorized:', `${message.author.username}#${message.author.discriminator}`)
+    .addField('Reason', reason);
+  return guild.channels.get(log.id).sendEmbed(embed);
+};
+
+};
