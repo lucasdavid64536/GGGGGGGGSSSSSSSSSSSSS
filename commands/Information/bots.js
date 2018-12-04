3a9a4d28fd22781f587b1417bad2139f48e43cc4
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-04 13:43:46 +0100

    Create bots.js

---
+++ commands/Information/bots.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    let bicon = bot.user.displayAvatarURL;
+    let botembed = new Discord.RichEmbed()
+    .setTitle("**Bot Information**")
+    .setDescription("**Bot's Informations**")
+    .setColor("#15f153")
+    .setThumbnail(bicon)
+    .addField("**• Bot Name**", bot.user.username)
+    .addField("**• Bot's ID**", "**517276995269296129**")
+    .addField(":prince:**• Bot's Owner**:prince: ", "***Ban#9153***")
+    .addField(":champagne: **• Ping** :ping_pong:", `**${Math.round(bot.ping)}Ms**`)
+    .addField(":calendar_spiral:**• Created On**:calendar_spiral:", "**Wed Nov 28 2018 09:54:06 GMT+0000 (Coordinated Universal Time)**")
+    .addField("**• Mem Usage**", "**145.35 / 1956.89 MB**")
+    .addField("**• Node.js:**", "**v10.13.0**")
+    .addField("**• Discord.js:**","**v11.4.2**")
+    .addField(":medal:**• Servers**:medal:", `**${bot.guilds.size}**`)
+    .addField(":busts_in_silhouette:**• Members**:busts_in_silhouette:", `**${bot.users.size}**`)
+    .addField(":video_game:**• Commands**:video_game:", `**${bot.commands.size}**`)
+
+    message.channel.send(botembed);
+}
+module.exports.help = {
+  name:"bot"
+}
