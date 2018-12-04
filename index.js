44b032425f9df20487cf1d81e0a8ccf71238f202
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-04 10:48:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ Discord = require("discord.js");
 const client = new Discord.Client();
 require('./util/cmdloader.js')(client);//requires the command loader
 const fs = require("fs");
+bot.commands = new Discord.Collection();
 let token = process.env.token;
 let prefix = ".";
 
@@ -27,6 +28,44 @@ client.user.setActivity(`${client.guilds.size} Servers With ${client.users.size}
     }      //seconds/1000 = ms
 
 client.on('message', message => {
+         if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
+
+  let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
+  if(!prefixes[message.guild.id]){
+    prefixes[message.guild.id] = {
+      prefixes: botconfig.prefix
+    };
+  }
+
+  if(!coins[message.author.id]){
+    coins[message.author.id] = {
+      coins: 0
+    };
+  }
+
+  let prefix = prefixes[message.guild.id].prefixes;
+  if(!message.content.startsWith(prefix)) return;
+  if(cooldown.has(message.author.id)){
+    message.delete();
+    return message.reply("You have to wait 5 seconds between commands.")
+  }
+  if(!message.member.hasPermission("ADMINISTRATOR")){
+    cooldown.add(message.author.id);
+  }
+
+
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+  let commandfile = bot.commands.get(cmd.slice(prefix.length));
+  if(commandfile) commandfile.run(bot,message,args);
+
+  setTimeout(() => {
+    cooldown.delete(message.author.id)
+  }, cdseconds * 1000)
+        
  if (message.author.bot) return;
  if (!message.content.startsWith(prefix)) return;
 
