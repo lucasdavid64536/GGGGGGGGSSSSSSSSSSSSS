eef243853c59ae0a1dfa3a32b1c3057c405fe563
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-04 13:09:21 +0100

    Create iD.js

---
+++ commands/Information/iD.js
@@ -0,0 +1,16 @@
+let Discord = require('discord.js');
+
+exports.run = (client, message) => {
+let cmd = message.content.split(' ').slice(1, 2).join(' ');
+	
+	
+  let memberToFind = message.mentions.members.first();
+
+  if (!memberToFind) {
+    return message.channel.send('**You Must Mention A User First To Use This Command** ***Expample :*** ``.id @User``');
+  }
+  
+   var embed = new Discord.RichEmbed()
+   .setTitle("**Member ID:**")
+   .setDescription("**This Member ID Is:**", memberToFind.id, true)
+  
