b486a5714a1d81493e21b7cc4ce27743b5120b2f
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-04 13:47:36 +0100

    Update info.js

--- commands/Information/info.js
+++ commands/Information/info.js
@@ -22,36 +22,6 @@ let cmd = message.content.split(' ').slice(1, 2).join(' ');
   message.channel.send(embed);
   message.react("✅");
 }	
-if(cmd === 'sv'){
-let embed = new Discord.RichEmbed()
-  .setColor("0x#F605DA")
-  .setTitle('**Server Info**')
-  .addField("Id", message.guild.id)
-  .addField("Owner", message.guild.owner)
-  .addField("Owner Id", message.guild.ownerID)
-  .addField("Roles", message.guild.roles.size)
-  .addField("Total Members", message.guild.memberCount)
-  .addField("Region", message.guild.region)
-.setThumbnail(message.guild.iconURL)
-message.channel.send(embed)
-message.react('ðŸ‘');
-	
-}
-if(cmd === 'server'){
-let embed = new Discord.RichEmbed()
-  .setColor("0x#F605DA")
-  .setTitle('**Server Info**')
-  .addField("Id", message.guild.id)
-  .addField("Owner", message.guild.owner)
-  .addField("Owner Id", message.guild.ownerID)
-  .addField("Roles", message.guild.roles.size)
-  .addField("Total Members", message.guild.memberCount)
-  .addField("Region", message.guild.region)
-.setThumbnail(message.guild.iconURL)
-message.channel.send(embed)
-message.react('ðŸ‘');
-	
-}
 
 module.exports.help = {
 	name: "whois",
