ac46b84bdb462bc426741b30f02d7a725aa4e455
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-04 12:48:17 +0100

    Update info.js

--- commands/Information/info.js
+++ commands/Information/info.js
@@ -12,7 +12,7 @@ let cmd = message.content.split(' ').slice(1, 2).join(' ');
 
   var embed = new Discord.RichEmbed()
   .setAuthor(memberToFind.user.tag, memberToFind.user.avatarURL)
-  .setThumbnail(user.displayAvatarURL)
+  .setImage(user.displayAvatarURL)
   .addField("Member ID:", memberToFind.id, true)
   .addField("Status", memberToFind.status, true)
   .addField('Account Created', memberToFind.user.createdAt, true)
