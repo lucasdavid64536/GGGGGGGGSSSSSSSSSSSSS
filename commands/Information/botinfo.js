91547fc27f3f495ff3a78e85ba9b1b5f4779e996
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-04 12:08:21 +0100

    Update botinfo.js

--- commands/Information/botinfo.js
+++ commands/Information/botinfo.js
@@ -6,8 +6,15 @@ module.exports.run = async (bot, message, args) => {
     .setDescription("**Bot Information**")
     .setColor("#15f153")
     .setThumbnail(bicon)
-    .addField("Bot Name", **bot.user.username**)
-    .addField("Created On", **bot.user.createdAt**)
+    .addField("**• Bot Name**", bot.user.username)
+    .addField("**• Created On**", bot.user.createdAt)
+    .addField("**• Mem Usage**", "145.35 / 1956.89 MB")
+    .addField("**• Uptime**", `${hours} hours, ${minutes} minutes and ${seconds} seconds`)
+    .addField("**• Node.js:**", "v10.13.0")
+    .addField("**• Discord.js:**","v11.4.2")
+    .addField("**• Servers**", `${client.guilds.size}`)
+    .addField("**• Members**", `${client.users.size}`)
+    .addField("**• Commands**", `${client.commands.size}`)
 
     message.channel.send(botembed);
 }
