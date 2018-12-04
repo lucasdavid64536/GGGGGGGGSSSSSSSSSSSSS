63319674bb8d0de1acf318bf5bb13256e35d34ae
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-04 11:02:20 +0100

    Update botinfo.js

--- commands/Information/botinfo.js
+++ commands/Information/botinfo.js
@@ -9,11 +9,12 @@ module.exports.run = async (bot, message, args) => {
     .addField('**Bot Name**', bot.user.username)
     .addField('**My Owner**',"```Ban#9153```")
     .addField('**Discord.js**',"```v11.4.2```")
-    .addField('**Created On**', `**bot.user.createdAt**`)
-    .addField('**Servers Bot Is In Them**', `**${bot.guilds.size}**`)
-    .addField('**Watching Member**', `**${bot.users.size}**`)
-    .addField('**Playing With Bots**', `**${bot.bots.size}**`)
-    .addField('**Up Time**', `**${hours} hours, ${minutes} minutes and ${seconds} seconds**`)
+    .addField('Node.js',"```v10.13.0```")
+    .addField('**xBot#3901 was Created on Dec-11-2017**', `**bot.user.createdAt**`)
+    .addField('**Servers Bot Is In Them**', `${bot.guilds.size}`)
+    .addField('**Watching Member**', `${bot.users.size}`)
+    .addField('**Playing With Bots**', `${bot.bots.size}`)
+    .addField('**Up Time**', `${hours} hours, ${minutes} minutes and ${seconds} seconds`)
     
 
     message.channel.send(botembed);
