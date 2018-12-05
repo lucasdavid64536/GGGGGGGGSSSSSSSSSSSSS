fe94502f16f30e83e131ffcff16c35eb2cc5ad90
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-05 19:51:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ client.user.setActivity(`${client.guilds.size} Servers With ${client.users.size}
     }      //seconds/1000 = ms
      function game4() {
        client.user.setActivity(`Buying Mojang Premium Accounts From https://selly.gg/@BanGamer For Only 4€ Do Like Me! `, { type: "Impressed" });
-        setTimeout(game1, 20000);//these times are in ms, so 30,000 = 30 seconds
+        setTimeout(game4, 30000);//these times are in ms, so 30,000 = 30 seconds
     } 
 client.on('message', message => {
  if (message.author.bot) return;
