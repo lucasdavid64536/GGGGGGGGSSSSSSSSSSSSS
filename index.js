108e35c2e7f596d764ff72b17037c3a28c4544e2
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-06 14:30:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ client.user.setActivity(`${client.guilds.size} Servers With ${client.users.size}
     }
     
     function game2() {
-        client.user.setActivity(`Other Bots`, { type: "Watching" });
+        client.user.setActivity(`Mojang Premium Accounts From https://selly.gg/@BanGamer For Only 4€ Do Like Me!`, { type: "Buying" });
         setTimeout(game3, 20000)
     }
     
@@ -24,10 +24,6 @@ client.user.setActivity(`${client.guilds.size} Servers With ${client.users.size}
        client.user.setActivity(`Responding For ${client.commands.size} commands`, { type: "Watching" });
         setTimeout(game1, 20000);//these times are in ms, so 30,000 = 30 seconds
     }      //seconds/1000 = ms
-     function game4() {
-       client.user.setActivity(`Buying Mojang Premium Accounts From https://selly.gg/@BanGamer For Only 4€ Do Like Me! `, { type: "Impressed" });
-        setTimeout(game4, 30000);//these times are in ms, so 30,000 = 30 seconds
-    } 
 client.on('message', message => {
  if (message.author.bot) return;
  if (!message.content.startsWith(prefix)) return;
