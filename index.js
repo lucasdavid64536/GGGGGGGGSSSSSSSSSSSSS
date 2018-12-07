752fbc0613046a4d488c12e502ead38dd7c0bced
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-07 12:32:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,25 @@ const fs = require("fs");
 let token = process.env.token;
 let prefix = ".";
 
+
+fs.readdir("./commands/", (err, files) => {
+
+  if(err) console.log(err);
+
+  let jsfile = files.filter(f => f.split(".").pop() === "js")
+  if(jsfile.length <= 0){
+    console.log("Couldn't find commands.");
+    return;
+  }
+
+  jsfile.forEach((f, i) =>{
+    let props = require(`./commands/${f}`);
+    console.log(`${f} loaded!`);
+    bot.commands.set(props.help.name, props);
+  });
+
+});
+
 client.on('ready', () => {
 console.log('IM READY !');
 client.user.setActivity(`${client.guilds.size} Servers With ${client.users.size} Members`, { type: "Watching" });
