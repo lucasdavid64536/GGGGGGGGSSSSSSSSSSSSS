7d38d118eb0d59118387771173facc91d4fa8a9b
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-08 09:44:21 +0100

    Update game.js

--- commands/Misc/game.js
+++ commands/Misc/game.js
@@ -6,7 +6,7 @@ exports.run = (client, message) => {
   .setTitle("**Our Community Games & Events**")
   .setDescription("**Hey Hello ! Welcome Our Community Our Games Are http://starve.io/ - https://www.scrible.com/ - https://www.lordz.io/ - https://moomoo.io/ - there more games ask Ban about them (The Leader and The Owner Of me)**")
   .setColor("0x#F8FC05")
-  .setFooter("Have A Good Time With Testing And Using my Awesome Commands ! a Special Thanks From  Ban For Using Hes Bot ")
+  .setFooter("Need Support ? Join Our Support Community And Get Help There https://discord.gg/38C2CDU ")
   message.channel.sendEmbed(embed);
   message.react('ðŸ‘');
 	
