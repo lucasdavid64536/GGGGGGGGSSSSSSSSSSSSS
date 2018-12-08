1cf5e44adc4d83898835b9b8a3d07a5ff3aec872
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-08 09:46:40 +0100

    Update invite.js

--- commands/Misc/invite.js
+++ commands/Misc/invite.js
@@ -6,7 +6,7 @@ exports.run = (client, message) => {
   .setTitle("**Want To Me In Your Server ? Awesome !**")
   .setDescription("**You Can Find Me With This** [Link](https://discordapp.com/oauth2/authorize?client_id=517276995269296129&permissions=8&scope=bot)")
   .setColor("0x#220A29")
-   .setFooter("Get Mojang & Spotify Premium Accounts ! With only 4 Euro Just Visit https://selly.gg/@BanGamer And Buy 2 Mojang Premium Accounts With Only 4 Euro ! not 42$ Only 4 Euro!")
+   .setFooter("Need Support ? Join Our Support Community And Get Help There https://discord.gg/38C2CDU")
   message.channel.sendEmbed(embed);
   message.react('ðŸ‘');
 	
