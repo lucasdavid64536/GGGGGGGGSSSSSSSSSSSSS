5fa41cd54140dd73c39b1289baa0abfc1717a5af
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-08 09:47:21 +0100

    Update youtube.js

--- commands/Misc/youtube.js
+++ commands/Misc/youtube.js
@@ -6,7 +6,7 @@ exports.run = (client, message) => {
   .setTitle("**iHaxBan Youtube Channel**")
   .setDescription("**Hi And Welcome Im xBot The Bot Of Ban Need An Awesome Role ? Just Subscribe To Him With This Link And Get @Subscriber role Its Easy Right ? https://www.youtube.com/channel/UC4nzXiKezdyEHgJaBYHrK6Q?view_as=subscriber** **You Can Also Learn How to Code A Bot Like Me By Watching Ban's Videos !**")
   .setColor("0x#DF0101")
-  .setFooter("Have A Good Time With Testing And Using my Awesome Commands ! a Special Thanks From  Ban For Using Hes Bot ")
+  .setFooter("Need Support ? Join Our Support Community And Get Help There https://discord.gg/38C2CDU")
   message.channel.sendEmbed(embed);
   message.react('ðŸ‘');
 	
