b2682c696ddfde6d95deac96762e4a6b5e7d0c52
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-08 15:07:02 +0100

    Create testd.js

---
+++ commands/Misc/testd.js
@@ -0,0 +1,12 @@
+let Discord = require('discord.js');
+
+exports.run = (client, msg) => {
+	var embed = new Discord.RichEmbed()
+.setTitle("**xBot Fun Commands**")
+.setDescription("test")
+ dUser.send(Embed)
+ }
+module.exports.help = {
+	name: "test",
+	alias: "TEST"
+}
