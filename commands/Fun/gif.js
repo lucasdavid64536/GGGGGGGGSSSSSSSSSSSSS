fdc717a1c881584b75a50ab74c74a1dc8f168255
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-12 16:30:32 +0100

    Update gif.js

--- commands/Fun/gif.js
+++ commands/Fun/gif.js
@@ -2,11 +2,7 @@
  
 exports.run = async (client, message, args1) => { 
   var replys = [
-    "https://media.giphy.com/media/xTiTnHXbRoaZ1B1Mo8/giphy.gif",
-          "https://media.giphy.com/media/hPPx8yk3Bmqys/giphy.gif",
-          "https://media.giphy.com/media/wJNGA01o1Zxp6/giphy.gif",
-          "https://media.giphy.com/media/TIyJGNK325XGciFEnI/giphy.gif",
-          "https://media.giphy.com/media/O1GhSbro4z4Dm/giphy.gif",
+    "https://giphy.com/gifs/1fl3uoVMsx9mFkECau",
 
           ];
           let gif = (replys[Math.floor(Math.random() * replys.length)])
