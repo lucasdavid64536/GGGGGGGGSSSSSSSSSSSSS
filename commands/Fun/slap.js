94d2ae0d89a4f5ab561e0ce717d8e5b50d14fdc3
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-12 17:00:32 +0100

    Update slap.js

--- commands/Fun/slap.js
+++ commands/Fun/slap.js
@@ -6,19 +6,11 @@ let memberToFind = message.mentions.members.first();
   if (!memberToFind) {
     return message.channel.send('**You Must Mention A User First To Use This Command** ***Expample :*** ``.slap @User``');
   }
- var replys = [
-	 "https://media.giphy.com/media/Asm8nZCbWC4ptgDbDe/giphy.gif",
-	 "https://media.giphy.com/media/SHhUwqxvWRgg6TL2yu/giphy.gif",
-	 
-	 ];
-	
-	
-	let gif = (replys[Math.floor(Math.random() * replys.length)])
+
           var embed = new Discord.RichEmbed()
 	  .setColor("RANDOM")
-	  .setImage(gif)
-	  .setTitle(`***slaps ${message.mentions.members.first()}***`)
-	  .setFooter("RIP")
+	  .setTitle("RIP")
+	  .setFooter(`***slaps ${message.mentions.members.first()}***`)
 	  
     message.channel.send(embed)
 }
