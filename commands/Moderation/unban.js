a83b39d23c2a24dd9f8daffbc68f8338121a9a73
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-14 16:11:03 +0100

    Update unban.js

--- commands/Moderation/unban.js
+++ commands/Moderation/unban.js
@@ -5,7 +5,7 @@ exports.run = (bot, message, args) => {
     const user = args[0];
     const modlog = bot.channels.find('name', 'log');
     if (!modlog) return message.reply('**I Cant Find Log Channel Please Create A Channel With** ``log`` **Name To Be Able To Use This Command**');
-    if (reason.length < 1) return message.reply('*You Did Not Specify The Reason For The Unban **');
+    if (reason.length < 1) return message.reply('**You Did Not Specify The Reason For The Unban **');
     if (!user) return message.reply('**I Cant Find User ! Please Type Hes** ``__ID__`` **And This Command Will Work**').catch(console.error);
     message.guild.unban(user);
     message.reply(`Successfuly unbanned <@${user}>`)
