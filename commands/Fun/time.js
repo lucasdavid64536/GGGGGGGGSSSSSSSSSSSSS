18dbd9c90b5013cdf6cdb617ddd2cca54bca7c01
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-15 15:20:00 +0100

    Create time.js

---
+++ commands/Fun/time.js
@@ -0,0 +1,22 @@
+
+let Discord = require('discord.js');
+
+exports.run = (client, message) => {
+
+
+
+var today = new Date()
+let Day = today.toString().split(" ")[0].concat("day");
+let Month = today.toString().split(" ")[1]
+let Year = today.toString().split(" ")[3]
+const embed = new Discord.RichEmbed()
+      .setColor(`RANDOM`)
+.addField("Today is", `\`${Day}\` ,\`${Month}\` ,\`${Year}\`\n\`Time of day:\` \`${today.toString().split(" ")[4]}\``)
+message.channel.send({ embed })
+    message.react("🕰")   
+};
+
+module.exports.help = {
+	name: "time"
+  alias: "t"
+}
