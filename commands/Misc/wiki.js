0221b4f58b700a394fb239870ab46bad2e3d92a1
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-15 15:36:08 +0100

    Create wiki.js

---
+++ commands/Misc/wiki.js
@@ -0,0 +1,14 @@
+let Discord = require('discord.js');
+
+exports.run = (client, message) => {
+
+
+
+    let google = args.slice(1).join('+');
+    let link = `https://en.wikipedia.org/wiki/` + wiki;
+	message.channel.send(link);
+}
+    
+module.exports.help = {
+	name: "wiki"
+}
