aadbc55373bbcd4143a612e9512237a8c5d8d0e2
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-15 15:12:23 +0100

    Update invite.js

--- commands/Misc/invite.js
+++ commands/Misc/invite.js
@@ -4,7 +4,7 @@ exports.run = (client, message) => {
 	
   var embed = new Discord.RichEmbed()
   .setTitle("**Want To Me In Your Server ? Awesome !**")
-  .setDescription("**You Can Find Me With This** [Link](https://discordapp.com/oauth2/authorize?client_id=517276995269296129&permissions=8&scope=bot)")
+  .setDescription("**You Can Find Me With This** [Invite Link](https://discordapp.com/oauth2/authorize?client_id=517276995269296129&permissions=8&scope=bot)")
   .setColor("0x#220A29")
    .setFooter("Need Support ? Join Our Support Community And Get Help There https://discord.gg/38C2CDU")
   message.channel.sendEmbed(embed);
