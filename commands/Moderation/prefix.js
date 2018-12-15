53499e842d4771025c404e48c4dd940e45c34b75
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-15 15:28:53 +0100

    Update prefix.js

--- commands/Moderation/prefix.js
+++ commands/Moderation/prefix.js
@@ -1,17 +1,27 @@
 const Discord = require('discord.js');
 
 exports.run = async (client, message, level) => {
-  
-    if (!message.member.hasPermission("ADMINISTRATOR" || !message.author.id === botconfig.ownerID)) {
-      let setprefix_error = new Discord.RichEmbed()
-      .setColor("#4999f0")
-      .setTitle(`Only users with **Administrator** permissions can use this!`)
-      message.channel.send(setprefix_error);
-      return;
-    };
-  }
+	
+  var prefix = '.';
+let fetched = await db.fetch(`prefix_${message.guild.id}`);
+if (fetched === null) prefix = 'your prefix here';
+else prefix = fetched;
 
-module.exports.help = {
-	name: "pre",
-	alias: "P",
-  }
+// code 2 [new command]
+
+if (!message.member.hasPermission('ADMINISTRATOR') && message.author.id !== '357555941215961099') return message.channel.send('Sorry, you don\'t have permission to change server prefix')
+	.then(msg => msg.delete({
+		timeout: 10000
+	}));
+if (!args.join(' ')) return message.channel.send('Please provide a prefix to change server prefix')
+	.then(msg => msg.delete({
+		timeout: 10000
+	}));
+
+db.set(`prefix_${message.guild.id}`, args.join(' '))
+	.then(i => {
+		message.channel.send(`Server Prefix has been changed to ${i}`);
+	})
+	module.exports.help = {
+	name: "prefix"
+}
