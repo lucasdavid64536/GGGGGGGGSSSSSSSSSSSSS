2c6ef57652b55d38540ffe787248879d56b798d5
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-15 13:30:39 +0100

    Update kick.js

--- commands/Moderation/kick.js
+++ commands/Moderation/kick.js
@@ -10,7 +10,7 @@ module.exports.run = async (bot, message, args) => {
     let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!kUser) return message.channel.send("**I Can't Find This User ! Mention The User To Be Able To Use This Command**");
     let kReason = args.slice(1).join(" ") || "None";
-    if(bUser.hasPermission("ADMINISTRATOR")) return message.channel.send("**This User Cant Be Kicked Because He Had A Role Highler Than You** ``/OR/`` **He Is The Owner Of This Server** ``/OR/`` **He Had Administrator Permission**");
+    if(kUser.hasPermission("ADMINISTRATOR")) return message.channel.send("**This User Cant Be Kicked Because He Had A Role Highler Than You** ``/OR/`` **He Is The Owner Of This Server** ``/OR/`` **He Had Administrator Permission**");
 
   
     let kickEmbed = new Discord.RichEmbed()
