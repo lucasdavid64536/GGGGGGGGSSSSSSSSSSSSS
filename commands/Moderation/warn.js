e5b64e62488e16c5d3c4fb6611ee378a8dd8651a
BanYT <44928120+BanYT@users.noreply.github.com>
2018-12-15 15:46:27 +0100

    Create warn.js

---
+++ commands/Moderation/warn.js
@@ -0,0 +1,23 @@
+let Discord = require('discord.js');
+
+const args = message.content.slice(prefix.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  module.exports.run = async (bot, message, args) => {
+
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Sorry, but you don't have permission to use this!") 
+   let warnedmember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+    if (!warnedmember) return ("Please mention a user to warn.");
+     let reason = args.slice(1).join(' ');
+    if(!reason) reason = "No reason provided";
+   
+    
+      message.delete().catch(O_o=>{});
+    message.channel.send(`***${warnedmember.user.tag} was warned!***`)
+   await warnedmember.send(`You have been warned in ${message.guild.name} by ${message.author.username} for: ${reason}.`)
+  
+  }
+  
+  module.exports.help = {
+	name: "warn",
+	alias:"WARN
+}
