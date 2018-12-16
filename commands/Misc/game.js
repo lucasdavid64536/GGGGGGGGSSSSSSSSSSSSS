5d1bb30098312b8f41d40ecf92e7f81789e1220d
lucasdavid64536 <38979101+lucasdavid64536@users.noreply.github.com>
2018-12-16 13:26:19 +0200

    Update game.js

--- commands/Misc/game.js
+++ commands/Misc/game.js
@@ -6,7 +6,7 @@ exports.run = (client, message) => {
   .setTitle("**Our Community Games & Events**")
   .setDescription("**Hey Hello ! Welcome Our Community Our Games Are http://starve.io/ - https://www.scrible.com/ - https://www.lordz.io/ - https://moomoo.io/ - there more games ask Ban about them (The Leader and The Owner Of me)**")
   .setColor("0x#F8FC05")
-  .setFooter("Need Support ? Join Our Support Community And Get Help There https://discord.gg/38C2CDU ")
+  .setFooter("Need Support ? Join Our Support Community And Get Help There https://discord.gg/UpjCWx9 ")
   message.channel.sendEmbed(embed);
   message.react('ðŸ‘');
 	
