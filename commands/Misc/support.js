75d1664007f259dc63af9b01c35e04877be9b69e
lucasdavid64536 <38979101+lucasdavid64536@users.noreply.github.com>
2018-12-16 13:28:50 +0200

    Update support.js

--- commands/Misc/support.js
+++ commands/Misc/support.js
@@ -4,9 +4,9 @@ exports.run = (client, message) => {
 	
   var embed = new Discord.RichEmbed()
   .setTitle("**Need Support ?**")
-  .setDescription("**You Can Join The Support Server https://discord.gg/93WKw7M Or You Can Only say help to see all my commands**")
+  .setDescription("**You Can Join The Support Server https://discord.gg/UpjCWx9 Or You Can Only say help to see all my commands**")
   .setColor("RANDOM")
-  .setFooter("Need Support ? Join Our Support Community And Get Help There https://discord.gg/38C2CDU")
+  .setFooter("Need Support ? Join Our Support Community And Get Help There https://discord.gg/UpjCWx9")
   message.channel.sendEmbed(embed);
   message.react('ðŸ‘');
 	
