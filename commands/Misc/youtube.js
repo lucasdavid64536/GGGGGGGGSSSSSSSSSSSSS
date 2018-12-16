dcc66a2f40f456421acd488bd2b2ee566fd98c70
lucasdavid64536 <38979101+lucasdavid64536@users.noreply.github.com>
2018-12-16 13:28:07 +0200

    Update youtube.js

--- commands/Misc/youtube.js
+++ commands/Misc/youtube.js
@@ -4,9 +4,9 @@ exports.run = (client, message) => {
 	
   var embed = new Discord.RichEmbed()
   .setTitle("**iHaxBan Youtube Channel**")
-  .setDescription("**Hi And Welcome Im xBot The Bot Of Ban Need An Awesome Role ? Just Subscribe To Him With This Link And Get @Subscriber role Its Easy Right ? https://www.youtube.com/channel/UC4nzXiKezdyEHgJaBYHrK6Q?view_as=subscriber** **You Can Also Learn How to Code A Bot Like Me By Watching Ban's Videos !**")
+  .setDescription("**Hi And Welcome Im Bot The Bot Of Ban Need An Awesome Role ? Just Subscribe To Him With This Link And Get @Subscriber role Its Easy Right ? https://www.youtube.com/channel/UC4nzXiKezdyEHgJaBYHrK6Q?view_as=subscriber** **You Can Also Learn How to Code A Bot Like Me By Watching Ban's Videos !**")
   .setColor("0x#DF0101")
-  .setFooter("Need Support ? Join Our Support Community And Get Help There https://discord.gg/38C2CDU")
+  .setFooter("Need Support ? Join Our Support Community And Get Help There https://discord.gg/UpjCWx9")
   message.channel.sendEmbed(embed);
   message.react('ðŸ‘');
 	
